59c40a241bd675f29f63244181c04b18d3104523
ssrijasdaruk <sirodom.srijasdaruk@accenture.com>
2022-10-26 14:02:09 +1000

    Update house_info.py

--- sensor/house_info.py
+++ sensor/house_info.py
@@ -6,7 +6,6 @@ class HouseInfo():
 
     def get_data_by_area(self, field, rec_area=0):
         field_data = []
-
         for record in self.data:
             if rec_area==0:
                 field_data.append(record[field])
@@ -16,7 +15,6 @@ class HouseInfo():
 
     def get_data_by_date(self, field, rec_date=datetime.today()):
         field_data = []
-
         for record in self.data:
             if rec_date.strftime("%m/%d/%y") == record['date']:
                 field_data.append(record[field])
