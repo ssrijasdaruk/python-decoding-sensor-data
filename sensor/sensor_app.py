8892b03d89e28db15e81f350738d3cce0c003082
ssrijasdaruk <sirodom.srijasdaruk@accenture.com>
2022-10-26 13:57:58 +1000

    update

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -24,9 +24,9 @@ print("Loaded records: {}".format(len(data)))
 
 # Module 5 code here:
 house_info = HouseInfo(data)
-#test_area = 1
-#recs = house_info.get_data_by_area("id", rec_area=test_area)
-#print("\nHouse sensor records for area {} = {}".format(test_area, len(recs)))
+test_area = 1
+recs = house_info.get_data_by_area("id", rec_area=test_area)
+print("\nHouse sensor records for area {} = {}".format(test_area, len(recs)))
 
 test_date = datetime.strptime("5/9/20", "%m/%d/%y")
 recs = house_info.get_data_by_date("id", rec_date=test_date)
