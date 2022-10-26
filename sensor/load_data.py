fdcedbe3f95003212f20833cb4b20f57a6e2e7ae
ssrijasdaruk <sirodom.srijasdaruk@accenture.com>
2022-10-26 11:49:50 +1000

    Update load_data.py

--- sensor/load_data.py
+++ sensor/load_data.py
@@ -7,6 +7,6 @@ def load_sensor_data():
     for sensor_file in sensor_files:
         with open(sensor_file, 'rt') as data_file:
             data_reader = csv.DictReader(data_file, delimiter=',')
-            for row in datafile:
+            for row in data_file:
                 sensor_data.append(row)
     return sensor_data
